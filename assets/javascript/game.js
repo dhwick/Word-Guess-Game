012bed909f541a26efd2f16bb54ddae488c4774d
dhwickgit config --global user.name dhwick$ git config --global user.namegit config <dhwick88@gmail.com>
2018-08-11 23:17:58 -0400

    this is hangman homework

---
+++ assets/javascript/game.js
@@ -0,0 +1,36 @@
+
+
+var chosenWord = ["concatenation", "driving", "typing", "coffee", "coding", "reagan", "madison", "danielle", "maple", "bella"];
+var computerGuess = chosenWord[Math.floor(Math.random() * chosenWord.length)];
+var wrongLetter =[];
+var correctLetters = [];
+var underScore = [];
+
+
+for (var i = 0; i < computerGuess.length; i++) {
+    underScore.push("_");
+}
+
+document.getElementById("#solve").innerHtml = underScore.push("_");
+
+
+document.onkeyup = function() {
+
+var userGuess = event.key; 
+
+
+
+var letters = computerGuess.split(""); 
+
+function
+
+if (userGuess === computerGuess.charAt(letters.length) {
+
+    //appear at underScore.push with should go in my paragraph element
+    
+}
+    //i don't understand why things are diplaying
+
+    //
+
+}
